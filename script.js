f091a003ce4a62f4fd27bdc58b8d4b8348d5a0ac
IAbdulSubhan <subhankhalidofficial@gmail.com>
2025-04-30 23:16:16 +0500

    minior fixes

---
+++ script.js
@@ -0,0 +1,10 @@
+function sendMail(){
+    let params = {
+        name : document.getElementById("name").value,
+        email : document.getElementById("email").value,
+        subject : document.getElementById("subject").value,
+        message : document.getElementById("message").value,
+    }
+
+    emailjs.send("service_mxqzz6v", "template_v329sya", params).then(alert("Email Sent!!"))
+}
